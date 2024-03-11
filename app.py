72276e242cc328230796455e1452423654b156e5
Javlon <javlonravshanov2002@gmaill.com>
2024-03-11 12:20:41 +0100

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from fastai.vision.all import *
+st.write("""
+# Transport classification model
+A model that identifies the type of transport from the image you have uploaded \n
+*P.S. Please upload only transport images, others are not supported*         
+""")
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+import plotly.express as px
+
+
+file = st.file_uploader('Upload image', type=['png', 'jpg', 'jpeg', 'gif', 'svg'])
+if file:
+    st.image(file)
+    img = PILImage.create(file)
+    model = load_learner('transport_types.pkl')
+
+    pred, pred_id, probs = model.predict(img)
+    st.success(f"Prediction: {pred}")
+    st.info(f"Probability: {probs[pred_id]*100:.2f}%")
+
+    # plotting
+    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
